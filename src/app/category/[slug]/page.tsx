45b2e07acf10558f129892dc28ceb8494185cc76
Diogo Evangelista <dhesousa77@gmail.com>
2023-10-19 17:23:36 -0300

    feat: add page to show category products

---
+++ src/app/category/[slug]/page.tsx
@@ -0,0 +1,42 @@
+import { CATEGORY_ICON } from "@/app/constants/category-item";
+import { Badge } from "@/components/ui/badge";
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+
+const CategoryProducts = async ({ params }: any) => {
+  const category = await prismaClient.category.findFirst({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      products: true,
+    },
+  });
+  if (!category) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-1 border-2 px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
+        {params.slug}
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {category.products.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrice(product)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryProducts;
