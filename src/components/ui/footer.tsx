87b38d19726458c34f73d0fcf63165cbcc5cf84f
Diogo Evangelista <dhesousa77@gmail.com>
2023-10-24 10:21:08 -0300

    feat: ensure recommended products are shown on product details page

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+    <div className="mt-5 bg-accent px-8 py-4 text-[0.625rem] opacity-75">
       © 2023 Copyright <span className="font-semibold">FSW Store</span>
     </div>
   );
