a2ebc49d8792f4f731fb340d05112eb6c6a6a24c
Diogo Evangelista <dhesousa77@gmail.com>
2023-11-13 20:21:52 -0300

    feat: ensure footer is responsive on desktop

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,8 +1,10 @@
 const Footer = () => {
   return (
-    <div className="mt-5 bg-accent px-8 py-4 text-[0.625rem] opacity-75">
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm">
+    <div className="mx-auto lg:container">
       © 2023 Copyright <span className="font-semibold">FSW Store</span>
     </div>
+  </div>
   );
 };
 
